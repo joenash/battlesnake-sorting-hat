e19af26dd7ed7339d0c60d919409c665ec9bc106
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-06-16 11:07:53 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/joenash/battlesnake-sorting-hat/commit/8cca543f81b660b39db5d9fa77c146989c1576d5

--- assets/data/Houses.ts
+++ assets/data/Houses.ts
@@ -9,15 +9,6 @@ export interface HouseInt {
 
 export const houses: HouseInt[] = [
   {
-    name: 'Aileen',
-    image: 'Aileen.png',
-    banner: 'AileenBanner.png',
-    description:
-      'Aileen is depending on the powers of the occult and a big cup of coffee to guarantee her victory!',
-    head: 'fang',
-    colour: '#2B398F',
-  },
-  {
     name: 'Joe',
     image: 'Joe.png',
     banner: 'JoeBanner.png',
@@ -25,21 +16,5 @@ export const houses: HouseInt[] = [
       'Joe is completely fueled by carbs and promises ramen to all his supporters.',
     head: 'sand-worm',
     colour: '#930318',
-  },
-  {
-    name: 'Chris',
-    image: 'Chris.png',
-    banner: 'ChrisBanner.png',
-    description: 'Chris is here to get comfortable and make friends.',
-    head: 'beluga',
-    colour: '#316000',
-  },
-  {
-    name: 'Curtis',
-    image: 'Curtis.png',
-    banner: 'CurtisBanner.png',
-    description: 'We all know Curtis is cursed and he has decided to own it and become the bad boy of the group.',
-    head: 'evil',
-    colour: '#622BAA',
-  },
+  }
 ];
